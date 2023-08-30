7b87176ddf0103d960c9eaefa66ebf74f7a6d434
MertGunduz <merttgg@gmail.com>
2023-08-30 08:30:30 +0300

    aria2Download.c file comments and other stuff edited

--- src/yiffysearch/aria2Download.c
+++ src/yiffysearch/aria2Download.c
@@ -1,7 +1,9 @@
 /**
  * @file aria2Download.c
  * 
- * @brief downloads the API response by using aria2c, works with a system call
+ * @brief Downloads the JSON file by using aria2c, works with the system function. 
+ * 
+ * This file is used to set the request string by checking isNSFW parameter, and downloads the JSON file from e621/e926 API by using aria2c.
  * 
  * @author Mehmet Mert Gunduz
  * 
@@ -11,9 +13,9 @@
 #include "yiffy-search.h"
 
 /// @brief downloads the API response by using aria2c, works with a system call
-/// @param tags 
-/// @param page 
-/// @param isNsfw 
+/// @param tags These are the e621-e926 tags prompted by the user as an argument value. Example: yiffy --fetch "anthro+fur+male+smile".
+/// @param page This is the value that is passed to the API to get results from the specified pages.
+/// @param isNsfw This is the boolean value that checks the NSFW option. If true, sets the request string for e621 otherwise sets for e926.
 void aria2Download(char *tags, int page, bool isNsfw)
 {
     char *requestString = (char*)malloc(256 * sizeof(char));
@@ -24,6 +26,7 @@ void aria2Download(char *tags, int page, bool isNsfw)
         exit(EXIT_FAILURE);
     }
 
+    /* Set the string for NSFW or SFW option by checking the isNsfw parameter. */
     if (isNsfw)
     {
         sprintf(requestString, "aria2c \"https://e621.net/posts.json?limit=20&page=%d&tags=%s\" -o posts.json >/dev/null 2>&1", page, tags);
@@ -33,7 +36,7 @@ void aria2Download(char *tags, int page, bool isNsfw)
         sprintf(requestString, "aria2c \"https://e926.net/posts.json?limit=20&page=%d&tags=%s\" -o posts.json >/dev/null 2>&1", page, tags);
     }
 
-    /* download the response and sleep for signals */
+    /* Download the JSON and set a sleep(1) for signals. */
     int success = system(requestString);
     sleep(1);
 
@@ -41,7 +44,6 @@ void aria2Download(char *tags, int page, bool isNsfw)
 
     if (success == BAD_SYSTEM_FUNC_CALL)
     {
-        /* gives information related to the system func call error */
         badSystemFuncErrorMessage();
         exit(EXIT_FAILURE);
     }
