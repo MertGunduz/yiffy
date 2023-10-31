36c0dcc947872dda22587e83fee19bffabff11c5
MertGunduz <merttgg@gmail.com>
2023-10-31 19:06:39 +0300

    posts part added

--- src/search/search.c
+++ src/search/search.c
@@ -25,8 +25,11 @@ void search(char *tags)
         exit(EXIT_FAILURE);
     }
 
+    /* Create the color. */
+    init_pair(1, COLOR_BLACK, COLOR_WHITE);
+
     /* Top bar part of TUI. */
-    WINDOW *top_window = newwin(1, COLS, 0, 0);
+    WINDOW *top_window = newwin(3, COLS -1, 0, 1);
 
     if (top_window == NULL)
     {
@@ -34,16 +37,36 @@ void search(char *tags)
         endwin();
         exit(EXIT_FAILURE);
     }
-    
-    init_pair(1, COLOR_BLACK, COLOR_WHITE);
-    
-    wbkgd(top_window, COLOR_PAIR(1));
+
+    wattron(top_window, A_BOLD);
+    box(top_window, 0, 0);
+    wattroff(top_window, A_BOLD);
 
     mvwprintw(top_window, 0, 1, "YIFFY");
-    mvwprintw(top_window, 0, COLS - 18, "PRESS F1 FOR HELP");
+    mvwprintw(top_window, 0, COLS - 23, "PRESS CTRL+H FOR HELP");
+    mvwprintw(top_window, 1, 2, "- E621.NET - NSFW SEARCH ON");
 
     refresh();
     wrefresh(top_window);
+
+    /* Posts part of TUI. */
+    WINDOW *posts_window = newwin(20, COLS - 1, 3, 1);
+
+    if (posts_window == NULL)
+    {
+        window_create_error_msg();
+        endwin();
+        exit(EXIT_FAILURE);
+    }
+
+    wattron(posts_window, A_BOLD);
+    box(posts_window, 0, 0);
+    wattroff(posts_window, A_BOLD);
+
+    mvwprintw(posts_window, 0, 1, "POSTS");
+
+    refresh();
+    wrefresh(posts_window);
 
     /* Close the search app. */
     getch();
