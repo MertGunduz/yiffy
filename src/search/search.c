e7e6523df59b905d03a11bff9e54941bd5b0bc39
MertGunduz <merttgg@gmail.com>
2023-11-01 08:41:35 +0300

    code readability added

--- src/search/search.c
+++ src/search/search.c
@@ -10,6 +10,10 @@
 
 #include "yiffy_search.h"
 
+static void create_top_window(WINDOW *win);
+static void create_posts_window(WINDOW *win);
+static void create_info_window(WINDOW *win);
+
 void search(char *tags)
 {
     /* Start the ncurses based terminal user interface. */
@@ -28,47 +32,77 @@ void search(char *tags)
     /* Create the color. */
     init_pair(1, COLOR_BLACK, COLOR_WHITE);
 
-    /* Top bar part of TUI. */
+    /* Top window part of TUI. */
     WINDOW *top_window = newwin(3, COLS -1, 0, 1);
-
-    if (top_window == NULL)
-    {
-        window_create_error_msg();
-        endwin();
-        exit(EXIT_FAILURE);
-    }
-
-    wattron(top_window, A_BOLD);
-    box(top_window, 0, 0);
-    wattroff(top_window, A_BOLD);
-
-    mvwprintw(top_window, 0, 1, "YIFFY");
-    mvwprintw(top_window, 0, COLS - 23, "PRESS CTRL+H FOR HELP");
-    mvwprintw(top_window, 1, 2, "- E621.NET - NSFW SEARCH ON");
-
-    refresh();
-    wrefresh(top_window);
-
-    /* Posts part of TUI. */
     WINDOW *posts_window = newwin(20, COLS - 1, 3, 1);
+    WINDOW *info_window = newwin(7, COLS - 1, 23, 1);
 
-    if (posts_window == NULL)
-    {
-        window_create_error_msg();
-        endwin();
-        exit(EXIT_FAILURE);
-    }
-
-    wattron(posts_window, A_BOLD);
-    box(posts_window, 0, 0);
-    wattroff(posts_window, A_BOLD);
-
-    mvwprintw(posts_window, 0, 1, "POSTS");
-
-    refresh();
-    wrefresh(posts_window);
+    /* Create the terminal user interface by calling functions. */
+    create_top_window(top_window);
+    create_posts_window(posts_window);
+    create_info_window(info_window);
 
     /* Close the search app. */
     getch();
     endwin();
+}
+
+static void create_top_window(WINDOW *win)
+{
+    if (win == NULL)
+    {
+        window_create_error_msg();
+        endwin();
+        exit(EXIT_FAILURE);
+    }
+
+    wattron(win, A_BOLD);
+    box(win, 0, 0);
+    wattroff(win, A_BOLD);
+
+    mvwprintw(win, 0, 1, "YIFFY");
+    mvwprintw(win, 0, COLS - 23, "PRESS CTRL+H FOR HELP");
+    mvwprintw(win, 1, 2, "- E621.NET - NSFW SEARCH ON");
+
+    refresh();
+    wrefresh(win);
+}
+
+static void create_posts_window(WINDOW *win)
+{
+    if (win == NULL)
+    {
+        window_create_error_msg();
+        endwin();
+        exit(EXIT_FAILURE);
+    }
+
+    wattron(win, A_BOLD);
+    box(win, 0, 0);
+    wattroff(win, A_BOLD);
+
+    mvwprintw(win, 0, 1, "POSTS");
+
+    refresh();
+    wrefresh(win);
+}
+
+static void create_info_window(WINDOW *win)
+{
+
+    if (win == NULL)
+    {
+        window_create_error_msg();
+        endwin();
+        exit(EXIT_FAILURE);
+    }
+
+    wattron(win, A_BOLD);
+    box(win, 0, 0);
+    wattroff(win, A_BOLD);
+
+    mvwprintw(win, 0, 1, "INFORMATION");
+
+    refresh();
+    wrefresh(win);
 }
