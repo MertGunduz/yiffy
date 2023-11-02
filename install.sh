a5104d987e5b0a0141748dbb3e50825d12263cbd
MertGunduz <merttgg@gmail.com>
2023-11-02 07:05:02 +0300

    install.sh fixed, no dep check for other distros

--- install.sh
+++ install.sh
@@ -1,70 +1,41 @@
 #!/bin/bash
 
-# Define the repository URL
+# Set the repository URL
 repository="https://github.com/MertGunduz/yiffy.git"
 
-# List of required packages
-required_packages=("git" "cmake" "gcc")
-
-# Function to check if a command exists
-command_exists() {
-  command -v "$@" > /dev/null 2>&1
-}
-
-# Function to install packages
-install_packages() {
-  echo "Installing required packages..."
-  if command_exists apt-get; then
-    sudo apt-get update
-    sudo apt-get install -y "${required_packages[@]}"
-  elif command_exists yum; then
-    sudo yum install -y "${required_packages[@]}"
-  else
-    echo "Unsupported package manager. Please install the required packages manually."
-    exit 1
-  fi
-  echo "Packages installed successfully!"
-}
-
 # Welcome message
-echo "This script will build yiffy from source."
+echo "This is an example install.sh file. It doesn't check for required packages needed to install yiffy."
+echo "Please ensure you have installed the necessary packages before proceeding. Check the GitHub page for more information."
 
-# Check for required packages
-for package in "${required_packages[@]}"; do
-  if ! command_exists "$package"; then
-    echo "Error: Missing required package: $package"
-    install_packages
-    break
-  fi
-done
+# Prompt user to continue
+read -p "This script will build yiffy from source. Do you want to continue? (y/n): " input
 
-# Ask the user if they want to continue
-read -p "Do you want to continue? (y/n)? " input
+# Main logic
+case "$input" in
+    n|N)
+        echo "Installation cancelled by user."
+        ;;
+    y|Y)
+        echo "Starting installation..."
 
-# Main installation process
-case $input in
-  [Yy]* )
-    echo "Starting..."
-    if git clone "$repository"; then
-      cd yiffy || exit
-      mkdir -p build && cd build || exit
-      if cmake .. && make; then
-        sudo cmake --install .
-        echo "yiffy installed successfully!"
-      else
-        echo "Error: Build failed."
-        exit 1
-      fi
-    else
-      echo "Error: git clone failed."
-      exit 1
-    fi
-    ;;
-  [Nn]* )
-    echo "Installation aborted by the user."
-    ;;
-  * )
-    echo "Invalid input, please run the script again and enter y or n."
-    exit 1
-    ;;
+        # Clone the repository
+        if git clone "$repository"; then
+            cd yiffy || exit 1
+            mkdir -p build && cd build || exit 1
+            
+            # Configure and build the project
+            if cmake .. && make; then
+                # Install the project
+                sudo cmake --install .
+                echo "Installation completed successfully."
+            else
+                echo "Error: Build failed. Please check the output for details."
+            fi
+        else
+            echo "Error: Unable to clone the repository. Please check your internet connection and try again."
+        fi
+        ;;
+    *)
+        echo "Invalid input. Please run the script again and enter 'y' to continue or 'n' to cancel."
+        ;;
 esac
